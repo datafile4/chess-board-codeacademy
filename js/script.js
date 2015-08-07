c69bdba7e305b6da23f4c5d9c4e95f8bb39474cd
Emil Alasgarov <debianmik@gmail.com>
2015-08-07 10:34:16 +0500

    Changed Everything

--- js/script.js
+++ js/script.js
@@ -5,12 +5,24 @@ function Cell(x,y,figure){
   this.figure = figure;
 }
 
+function Figure(x,y,color){
+  this.x = x;
+  this.y = y;
+  this.color = color;
+}
 //constructors
 
+function King(x,y,color){
+  Figure.call(this,x,y,color);
+}
+
+King.prototype.movenment = function(endX,endY){
+  
+}
 
 
 var container = document.createElement("DIV");//big container
-$(container).addClass("colorGrey");
+
 $(container).addClass("container");
 document.body.appendChild(container);
 
@@ -23,8 +35,11 @@ for(var i=0; i<8; i++){
     divCells[i][j] = document.createElement("DIV"); //j - column
     divCells[i][j].setAttribute("class", "cell");
     if((i+j)%2!=0){ //paint cells black
-      $(divCells[i][j]).addClass("colorBlack");
+      $(divCells[i][j]).addClass("colorSilver");
+    } else{
+      $(divCells[i][j]).addClass("colorWhiteSilver");
     }
+    divCells[i][j].innerHTML = "&#9812";
     container.appendChild(divCells[i][j]);
   }
 }
